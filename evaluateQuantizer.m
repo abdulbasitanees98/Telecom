55fb8e3a77df25b9059acacf2355978d525aa6b4
Abdul Basit Anees <50206820+abdulbasitanees98@users.noreply.github.com>
2020-04-27 11:38:40 +0300

    Delete evaluateQuantizer.m

--- evaluateQuantizer.m
+++
@@ -1,8 +0,0 @@
-function [D, SQNR] = evaluateQuantizer(voice, ai, xi)
-    xq = zeros(length(voice),1);
-    for i = 1:N
-        xq((voice < ai(i+1))&(voice >= ai(i))) = xi(i);
-    end
-    D = mean((voice-xq).^2);
-    SQNR = mean(voice.^2)/D2;
-end
